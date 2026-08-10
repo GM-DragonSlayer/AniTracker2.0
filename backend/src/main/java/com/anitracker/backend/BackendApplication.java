e3cfe840965c75135f95057b82f7fa81d52bdf15
Saourya <saourya2312005@gmail.com>
2026-08-10 21:35:19 +0530

    FINALY FIXED THE DAMNED LOCALHOST GHOST!!

--- backend/src/main/java/com/anitracker/backend/BackendApplication.java
+++ backend/src/main/java/com/anitracker/backend/BackendApplication.java
@@ -1,15 +1,25 @@
 package com.anitracker.backend;
 
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
-import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
+	// Pull the exact URI from your application.properties file
+	@Value("${spring.data.mongodb.uri}")
+	private String mongoUri;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public MongoClient mongoClient() {
+		return MongoClients.create(mongoUri);
 	}
 }
